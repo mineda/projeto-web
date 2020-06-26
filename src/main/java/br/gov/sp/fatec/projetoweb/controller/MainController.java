11e86d7e8de11298d1eca45a3d50b03d557c2029
Emanuel Mineda Carneiro <mineda@gmail.com>
2020-06-26 17:11:09 -0300

    Incluindo Controller

---
+++ src/main/java/br/gov/sp/fatec/projetoweb/controller/MainController.java
@@ -0,0 +1,20 @@
+package br.gov.sp.fatec.projetoweb.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainController extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+            throws ServletException, IOException {
+        resp.getWriter().print("Acesso OK!");
+    }
+    
+}
